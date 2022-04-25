9e1c2d91c15fc92cfa50305783e261bb4cc286d4
bosconiyodusaba <104379655+bosconiyodusaba@users.noreply.github.com>
2022-04-25 18:21:09 +0200

    Add files via upload

---
+++ bosco_codes.ino
@@ -0,0 +1,46 @@
+
+
+int temppin=A0;
+int fan=7;
+int tempvalue=0;
+int redled=2;
+int yellowled=6;
+
+void setup(){
+
+ 
+  pinMode(7,OUTPUT);
+  pinMode(temppin,INPUT);
+ pinMode(2,OUTPUT);
+ pinMode(6,OUTPUT);
+Serial.begin(9600);
+}
+void loop(){
+  
+ tempvalue=analogRead(temppin);
+ float tempcel= (tempvalue/1024.0)*500;
+ float tempfarh= (tempcel*9)/5 + 32;
+ if (tempcel>=25)
+ {
+  Serial.println("HIGH TEMPERATURE");
+  Serial.println ("FAN IS ON ");
+  digitalWrite(2,HIGH);
+  digitalWrite(6,LOW); 
+  digitalWrite(7,HIGH);
+ 
+
+ }
+    
+   
+   else {
+    Serial.println ("LOW TEMPERATURE ");
+     Serial.println("FAN IS OFF ");
+  digitalWrite(2,LOW);
+ digitalWrite(6,HIGH); 
+ digitalWrite(7,LOW);
+ 
+
+ 
+   }
+   
+  }
